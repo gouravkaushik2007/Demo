f778dea6c7b88065dd4b454ef81cfd9171430b09
Gourav Kaushik <gouravkaushik122@gmail.com>
2025-07-16 08:47:44 +0530

    modified

--- project.py
+++ project.py
@@ -4,11 +4,11 @@ a = -1
 guess = 1
 while(a != n):
     a = int(input("Guess a number: "))
-    guess += 1
     if(a<n):
+        guess += 1
         print("Higher number please!")
     elif(a>n):
+        guess += 1
         print("Lower number please!")
 
 print(f"You have guessed the number {n} correctly in {guess} attempts")
-
